845378e59c1097ab5ce7ae8dc679eeb9c11e7087
LucasBerce71 <lucas.berce@apinteractive.com.br>
2020-09-13 18:17:04 -0300

    Feat: release build directory

---
+++ build/precache-manifest.9019029bc6af0962c53efb77af1d9a48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bb112f6c9fa535f79a37c76426c72d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0614cd58fa33748f0c1",
+    "url": "/static/js/2.ffaf6dce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ffaf6dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8ddbef5ab11ae42d10",
+    "url": "/static/js/main.a70a64d9.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "fe82ec0011d3b27b10f6a2991e6ce6d2",
+    "url": "/static/media/github-background.fe82ec00.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
